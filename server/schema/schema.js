b1600307315e6ed3400a86e8c297c2631ba513ba
Samip <callmesamip@gmail.com>
2022-08-11 19:58:29 +1000

    Fetching Projects too now

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,7 @@
 import { projects, clients } from '.././sampleData.js';
 import {
 	GraphQLID,
+	GraphQLList,
 	GraphQLObjectType,
 	GraphQLSchema,
 	GraphQLString,
@@ -17,14 +18,44 @@ const ClientType = new GraphQLObjectType({
 	}),
 });
 
+const ProjectType = new GraphQLObjectType({
+	name: 'Projects',
+	fields: () => ({
+		id: { type: GraphQLID },
+		clientId: { type: GraphQLID },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString },
+		status: { type: GraphQLString },
+	}),
+});
+
 const RootQuery = new GraphQLObjectType({
 	name: 'RootQueryType',
 	fields: {
+		clients: {
+			type: new GraphQLList(ClientType), //List of Client Types.
+			resolve(parentValue, args) {
+				return clients;
+			},
+		},
 		client: {
 			type: ClientType,
 			args: { id: { type: GraphQLID } },
 			resolve(parentValue, args) {
 				return clients.find((client) => client.id === args.id);
+			},
+		},
+		projects: {
+			type: new GraphQLList(ProjectType), //List of Client Types.
+			resolve(parentValue, args) {
+				return projects;
+			},
+		},
+		project: {
+			type: ProjectType,
+			args: { id: { type: GraphQLID } },
+			resolve(parentValue, args) {
+				return projects.find((project) => project.id === args.id);
 			},
 		},
 	},

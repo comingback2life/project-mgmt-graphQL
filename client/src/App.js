37f426b69e9ec416cdef304c0379df14bebdbb11
Samip <callmesamip@gmail.com>
2022-08-12 02:22:34 +1000

    Projects being fetched, will come back to complete it

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Header } from './components/Header';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Clients } from './components/Clients';
 import { AddClientModal } from './Modal/AddClientModal';
+import { Projects } from './components/Projects';
 
 const cache = new InMemoryCache({
 	typePolicies: {
@@ -33,6 +34,8 @@ function App() {
 					<Header />
 					<AddClientModal />
 					<Clients />
+					<hr />
+					<Projects />
 				</div>
 			</ApolloProvider>
 		</>

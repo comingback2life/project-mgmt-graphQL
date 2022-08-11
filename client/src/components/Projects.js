9e71115c54973df9a545643f905c16492542751f
Samip <callmesamip@gmail.com>
2022-08-12 02:06:32 +1000

    Working on GET projects

---
+++ client/src/components/Projects.js
@@ -0,0 +1,6 @@
+import { Spinner } from './Spinner';
+import { useQuery } from '@apollo/client';
+import { GET_PROJECTS } from '../queries/projectQueries';
+export const Projects = () => {
+	return <div>Projects</div>;
+};

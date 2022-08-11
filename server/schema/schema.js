878f39821ce16d3a796605628ae36b884d3264a2
Samip <callmesamip@gmail.com>
2022-08-11 19:21:03 +1000

    GraphQL schema with the field can be returned now

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,1 +1,35 @@
-export const schema = () => {};
+import { projects, clients } from '.././sampleData.js';
+import {
+	GraphQLID,
+	GraphQLObjectType,
+	GraphQLSchema,
+	GraphQLString,
+} from 'graphql';
+
+//Client Type
+const ClientType = new GraphQLObjectType({
+	name: 'Client',
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		email: { type: GraphQLString },
+		phone: { type: GraphQLString },
+	}),
+});
+
+const schema = new GraphQLSchema({
+	query: new GraphQLObjectType({
+		name: 'RootQueryType',
+		fields: {
+			clients: {
+				type: ClientType,
+				args: { id: { type: GraphQLID } },
+			},
+		},
+		resolve(parentValue, args) {
+			return 'hello';
+		},
+	}),
+});
+
+export default schema;

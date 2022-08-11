0686ee583c79f56a5645f01dc0f001c58b351d4e
Samip <callmesamip@gmail.com>
2022-08-11 18:53:29 +1000

    initial route setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
+import { schema } from './schema/schema';
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-app.use('/graphql', graphqlHTTP({}));
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: process.env.NODE_ENV === 'development', //for use in development
+	})
+);
 app.listen(PORT, console.log(`Server running on PORT ${PORT}`));

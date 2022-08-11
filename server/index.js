2e2ff8f02875c4d3bfbf79aa6fafc508396143a6
Samip <callmesamip@gmail.com>
2022-08-11 18:50:45 +1000

    graphQL route setup and schema being setup now

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
+import 'dotenv/config';
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+const PORT = process.env.PORT || 8000;
+
 const app = express();
-const PORT = 8000;
+app.use('/graphql', graphqlHTTP({}));
+app.listen(PORT, console.log(`Server running on PORT ${PORT}`));

c1d2170aeb0e747f590fcd2b9d807ae517f86320
Samip <callmesamip@gmail.com>
2022-08-12 23:33:57 +1000

    Client Data being fetched to the Modal

--- client/src/Modal/AddProjectModal.js
+++ client/src/Modal/AddProjectModal.js
@@ -3,8 +3,10 @@ import { useState } from 'react';
 import { FaList } from 'react-icons/fa';
 import { GET_PROJECTS } from '../queries/projectQueries';
 import { GET_CLIENTS } from '../queries/clientQueries';
+import { Spinner } from '../components/Spinner';
 export const AddProjectModal = () => {
 	const [form, setForm] = useState({});
+	const { error, loading, data } = useQuery(GET_CLIENTS);
 	const handleOnChange = (e) => {
 		const { name, value } = e.target;
 		setForm({
@@ -25,86 +27,110 @@ export const AddProjectModal = () => {
 		// addClient(name, email, phone);
 		setForm('');
 	};
+	if (loading) return <Spinner />;
+	if (error) return <p>Something went wrong !</p>;
 	return (
 		<>
-			<button
-				type="button"
-				className="btn btn-primary"
-				data-bs-toggle="modal"
-				data-bs-target="#addProjectModal"
-			>
-				<div className="d-flex align-items-center">
-					<FaList className="icon" />
-					New Project
-				</div>
-			</button>
-
-			<div
-				className="modal fade"
-				id="addProjectModal"
-				tabIndex="-1"
-				aria-labelledby="addProjectModalLabel"
-				aria-hidden="true"
-			>
-				<div className="modal-dialog">
-					<div className="modal-content">
-						<div className="modal-header">
-							<h5 className="modal-title" id="addProjectModalLabel">
-								Add New Project
-							</h5>
-							<button
-								type="button"
-								className="btn-close"
-								data-bs-dismiss="modal"
-								aria-label="Close"
-							></button>
+			{!loading && !error && (
+				<>
+					<button
+						type="button"
+						className="btn btn-primary"
+						data-bs-toggle="modal"
+						data-bs-target="#addProjectModal"
+					>
+						<div className="d-flex align-items-center">
+							<FaList className="icon" />
+							New Project
 						</div>
-						<div className="modal-body">
-							<form onSubmit={handleOnSubmit}>
-								<div className="mb-3">
-									<label className="form-label">Name</label>
-									<input
-										type="text"
-										className="form-control"
-										name="name"
-										id="name"
-										onChange={handleOnChange}
-									/>
+					</button>
+
+					<div
+						className="modal fade"
+						id="addProjectModal"
+						tabIndex="-1"
+						aria-labelledby="addProjectModalLabel"
+						aria-hidden="true"
+					>
+						<div className="modal-dialog">
+							<div className="modal-content">
+								<div className="modal-header">
+									<h5 className="modal-title" id="addProjectModalLabel">
+										Add New Project
+									</h5>
+									<button
+										type="button"
+										className="btn-close"
+										data-bs-dismiss="modal"
+										aria-label="Close"
+									></button>
 								</div>
-								<div className="mb-3">
-									<label className="form-label">Description</label>
-									<textarea
-										className="form-control"
-										name="description"
-										id="description"
-										onChange={handleOnChange}
-									></textarea>
+								<div className="modal-body">
+									<form onSubmit={handleOnSubmit}>
+										<div className="mb-3">
+											<label className="form-label">Name</label>
+											<input
+												type="text"
+												className="form-control"
+												name="name"
+												id="name"
+												onChange={handleOnChange}
+											/>
+										</div>
+										<div className="mb-3">
+											<label className="form-label">Description</label>
+											<textarea
+												className="form-control"
+												name="description"
+												id="description"
+												onChange={handleOnChange}
+											></textarea>
+										</div>
+										<div className="mb-3">
+											<label className="form-label">Status</label>
+											<select
+												name="status"
+												id="status"
+												className="form-select"
+												onChange={handleOnChange}
+											>
+												<option value="new">Not Started</option>
+												<option value="progress">In progress</option>
+												<option value="completed">Completed</option>
+											</select>
+										</div>
+										<div className="mb-3 mt-2">
+											<label className="form-label">
+												<strong>Client</strong>
+											</label>
+											<select
+												name="clientId"
+												id="clientId"
+												className="form-select"
+												onChange={handleOnChange}
+											>
+												<option value="">Select Client</option>
+												{data.clients.map((client) => (
+													<option key={client.id} value={client.id}>
+														{client.name}
+													</option>
+												))}
+											</select>
+										</div>
+										<button
+											className="btn btn-primary"
+											type="submit"
+											data-bs-dismiss="modal"
+										>
+											Submit
+										</button>
+									</form>
 								</div>
-								<div className="mb-3">
-									<label className="form-label">Status</label>
-									<select
-										name="status"
-										id="status"
-										className="form-select"
-										onChange={handleOnChange}
-									>
-										<option value="new">Not Started</option>
-										<option value="progress">In progress</option>
-										<option value="completed">Completed</option>
-									</select>
-								</div>
-								<button
-									className="btn btn-primary"
-									type="submit"
-									data-bs-dismiss="modal"
-								>
-									Submit
-								</button>
-							</form>
+							</div>
 						</div>
 					</div>
-				</div>
-			</div>
+				</>
+			)}
 		</>
 	);
 };

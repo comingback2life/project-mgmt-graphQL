7547c5cd4fb06a2fce650e954dcb2ad543fdd6c5
Samip <callmesamip@gmail.com>
2022-08-13 02:08:58 +1000

    Requiring dotenv only in development mode

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-import 'dotenv/config';
+if (process.env.NODE_ENV === 'development') {
+	require('dotenv/config');
+}
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './schema/schema.js';
 import cors from 'cors';
 import { connectDatabase } from './config/dbConfig.js';
 const PORT = process.env.PORT || 8000;
-
 const app = express();
 connectDatabase();
 app.use(cors());

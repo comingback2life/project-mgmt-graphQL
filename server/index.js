594a971c8606342b2a2358dd51b447163bdae10d
Samip <callmesamip@gmail.com>
2022-08-13 02:29:03 +1000

    Always forget this in the node file

--- server/index.js
+++ server/index.js
@@ -4,10 +4,15 @@ import { graphqlHTTP } from 'express-graphql';
 import schema from './schema/schema.js';
 import cors from 'cors';
 import { connectDatabase } from './config/dbConfig.js';
+import path from 'path';
+
 const PORT = process.env.PORT || 8000;
 const app = express();
+const __dirname = path.resolve();
+
 connectDatabase();
 app.use(cors());
+app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 app.use(
 	'/graphql',
 	graphqlHTTP({

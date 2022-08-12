c301357c6c3a8c69d65644f91b7c54d92bfd0da1
Samip <callmesamip@gmail.com>
2022-08-12 23:59:08 +1000

    Wrote the mutation for project

--- client/src/mutations/projectMutations.js
+++ client/src/mutations/projectMutations.js
@@ -1,1 +1,28 @@
 import { gql } from '@apollo/client';
+
+const ADD_PROJECT = gql`
+	mutation addProject(
+		$name: String!
+		$description: String!
+		$status: ProjectStatus!
+		$clientId: ID!
+	) {
+		addProject(
+			name: $name
+			description: $description
+			status: $status
+			clienId: $clientID
+		) {
+			id
+			name
+			description
+			status
+			client {
+				id
+				name
+				email
+				phone
+			}
+		}
+	}
+`;
